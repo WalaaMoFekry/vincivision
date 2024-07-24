57d72bb0c9c5fce64a9755ae003c3b83aaa90877
Walaa Fekry <walaafekryai@gmail.com>
2024-07-24 16:54:42 +0300

    implement clerk webhooks

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -9,9 +9,7 @@ import { handleError } from "../utils";
 export async function createUser(user: CreateUserParams) {
   try {
     await connectToDatabase();
-
     const newUser = await User.create(user);
-
     return JSON.parse(JSON.stringify(newUser));
   } catch (error) {
     handleError(error);
